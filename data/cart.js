f18da863f380fe12a4f2ed9bc6bd7c3f3c399ee2
G33tHvBBer <135154926+G33tHvBBer@users.noreply.github.com>
2023-07-29 10:03:38 -0400

    Add files via upload

---
+++ data/cart.js
@@ -0,0 +1,46 @@
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+    let matchingItem;
+  
+    cart.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = item;
+    }})
+  
+    if (matchingItem) {
+      matchingItem.quantity++;
+    } else {
+      cart.push({
+        productId : productId,
+        quantity: 1
+      })
+    }
+
+    saveToStorage();
+  
+  }
+
+  export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    })
+
+    cart = newCart;
+
+    saveToStorage();
+
+  }
+  
